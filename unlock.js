3fe5f856930296e85b32de571f088fb57143ae97
Loulah <olaobadiekolola@gmail.com>
2026-05-13 14:54:15 +0100

    vide works properly now

--- unlock.js
+++ unlock.js
@@ -2,9 +2,9 @@
 
 const unlockTimes = {
   intro: new Date("2026-05-13T20:24:00+01:00"),
-  memories: new Date("2026-05-13T20:34:00+01:00"),
-  appreciation: new Date("2026-05-13T20:30:00+01:00"),
-  reveal: new Date("2026-05-13T20:35:00+01:00"),
+  memories: new Date("2026-05-12T20:34:00+01:00"),
+  appreciation: new Date("2026-05-12T20:30:00+01:00"),
+  reveal: new Date("2026-05-12T20:35:00+01:00"),
 };
 
 // Check if page is unlocked
